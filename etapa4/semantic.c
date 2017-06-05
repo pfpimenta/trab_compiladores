ce458890a7623f939220a5e4f5b68052fb80c29b
pfpimenta <pedrofolettopimenta@gmail.com>
2017-06-05 18:40:08 -0300

    inicio do semanticSetDeclarations(ASTREE* node)

--- etapa4/semantic.c
+++ etapa4/semantic.c
@@ -4,6 +4,16 @@ void semanticSetDeclarations(ASTREE* node)
 {
   int i=0;
   if(!node) return;
-  //for son in node->sons
-  //  semanticSetDeclarations(son);
+  for(i=0; i<MAX_SONS; i++)
+  {
+    semanticSetDeclarations(node->son[i]);
+  }
+  /* copiado da aula:
+  if(node->type == ASTREE_VARDEC){
+    if(node->symbol){
+      if(node->symbol->type == SYMBOL_TK_IDENTIFIER){
+        node->symbol->type = SYMBOL_VAR;
+      }
+    }
+  }*/
 }
