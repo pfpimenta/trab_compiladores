e7d319b877b347150dc15a0d7786fe72a872c7b0
predowtf <predowtf@hotmail.com>
2017-06-07 18:29:57 -0300

    tipo do retorno

--- etapa4/semantic.c
+++ etapa4/semantic.c
@@ -224,6 +224,25 @@ void semanticVardec(ASTREE* node){
   //printSymbol(*node->symbol);
 }
 
+
+void findReturn(ASTREE* node, int dataT)
+{
+    int i;
+    if(!node) return;
+    if (node->type == ASTREE_KWRETURN)
+    {  
+	if(getExprType(node->son[0]) == EXPR_BOOLEAN)     //Dúvida: os DataTypes são intercambiáveis? Exemplo: long a (byte x, long y) return x+y
+	{
+        fprintf(stderr, "ERRO SEMANTICO\nretorno invalido \n");
+        exit(4);
+        }
+    }
+    for(i=0; i < MAX_SONS; i++)
+    {
+        findReturn(node->son[i], dataT);
+    }
+}
+ 
 void semanticFuncdec(ASTREE* node)
 {
   if(node->symbol){
@@ -243,18 +262,23 @@ void semanticFuncdec(ASTREE* node)
     switch(node->son[0]->type){
       case ASTREE_KWBYTE:
         node->symbol->dataType = DATATYPE_BYTE;
+        findReturn(node->son[2], DATATYPE_BYTE);
         break;
       case ASTREE_KWSHORT:
         node->symbol->dataType = DATATYPE_SHORT;
+	findReturn(node->son[2], DATATYPE_SHORT);
         break;
       case ASTREE_KWLONG:
         node->symbol->dataType = DATATYPE_LONG;
+	findReturn(node->son[2], DATATYPE_LONG);
         break;
       case ASTREE_KWFLOAT:
         node->symbol->dataType = DATATYPE_FLOAT;
+	findReturn(node->son[2], DATATYPE_FLOAT);
         break;
       case ASTREE_KWDOUBLE:
         node->symbol->dataType = DATATYPE_DOUBLE;
+	findReturn(node->son[2], DATATYPE_DOUBLE);
         break;
       default:
         //fprintf(stderr, "ERRO QUE NAO DEVIA ACONTECER\n");
