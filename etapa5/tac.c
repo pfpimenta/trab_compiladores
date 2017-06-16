72a3e49b7abebba372a21da6b60c434dfa852755
pfpimenta <pedrofolettopimenta@gmail.com>
2017-06-15 22:26:52 -0300

    inicio tacGenerate

--- etapa5/tac.c
+++ etapa5/tac.c
@@ -3,10 +3,23 @@
 
 
 TAC* tacGenerate(ASTREE* node){
+  int i = 0;
+  TAC* code[MAX_SONS];
+  TAC* result = 0;
 
-  switch (node->type) {
-    case ASTREE_KWWHENTHEN:
-      //tacCreate()?
-      break;
+  if (!node) return 0;
+
+  //process children first
+  for(i=0; i<MAX_SONS; ++i)
+  {
+    code[i] = tacGenerate(node->son[i]);
   }
+  switch (node->type) {
+    case ASTREE_SYMBOL:
+      result = tacCreate(TAC_SYMBOL, node->symbol, 0, 0);
+      break;
+    default:
+      result = tacJoin( tacJoin( tacJoin(code[0], code[1]), code[2]), code[3]);
+  }
+  return result;
 }
