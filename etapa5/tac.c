38ef3475df86d953ed174347db9ba3db0c2144ec
predowtf <predowtf@hotmail.com>
2017-06-20 21:07:58 -0300

    makelabel e maketemp

--- etapa5/tac.c
+++ etapa5/tac.c
@@ -211,11 +211,24 @@ TAC* tacReverse(TAC* last)
 
 HASH_NODE* makeLabel()
 {
-
+  static int count = 0;
+  char* labelName = malloc(8 + sizeof(int) + 1);
+  
+  sprintf(labelName, "___label%d", count);
+  count++;
+  
+  return addSymbol(labelName, SYMBOL_IDENTIFIER,0,0);
 }
+
 HASH_NODE* makeTemporary()
 {
-
+  static int count = 0;
+  char* tempName = malloc(7 + sizeof(int) + 1);
+  
+  sprintf(tempName, "___temp%d", count);
+  count++;
+  
+  return addSymbol(tempName, SYMBOL_IDENTIFIER,0,0);
 }
 
 TAC* tacMakeWhen(ASTREE* node, TAC* code0, TAC* code1)
@@ -236,6 +249,11 @@ TAC* tacMakeWhenElse(ASTREE* node, TAC* code0, TAC* code1, TAC* code2)
   tacEndLabel = tacCreate(TAC_LABEL, endLabel,0, 0);
   return tacJoin(code0, tacJoin(tacIfz, tacJoin(code1, tacJoin(tacJmp,
     tacJoin(tacElseLabel, tacJoin(code2, tacEndLabel))))));
+}
+
+TAC* declaration_tac(TAC* id, TAC* literal)
+{
+   return tac(TAC_MOVE, id->destination, literal->destination, NULL);
 }
 
 TAC* tacGenerate(ASTREE* node){
@@ -260,8 +278,12 @@ TAC* tacGenerate(ASTREE* node){
     case ASTREE_KWWHENTHENELSE:
       result = tacMakeWhenElse(node, code[0], code[1], code[2]);
       break;
+    case ASTREE_KWRETURN:
+      result = tacJoin(code[0],tacCreate(TAC_RETURN, NULL, code[0]->res, NULL);
+      break; 
     default:
       result = tacJoin( tacJoin( tacJoin(code[0], code[1]), code[2]), code[3]);
   }
   return result;
 }
+
