80d1cad8a08018c20318c18157162ff9fc1f8a26
pfpimenta <pedrofolettopimenta@gmail.com>
2017-07-09 22:43:22 -0300

    switch case

--- etapa6/asm.c
+++ etapa6/asm.c
@@ -7,8 +7,39 @@ char* generateAsm (TAC* tac)
   // que eh o programa em assembly
   return "teste\n\n";
   switch (tac->type) {
+    case TAC_SYMBOL:
+      //ignora
+      break;
+    case TAC_RETURN:
+    case TAC_BEGGINFUN:
+    case TAC_ENDFUN:
+    case TAC_VECREAD:
+    case TAC_VECWRITE:
+    case TAC_MOV:
+    case TAC_READ:
+    case TAC_PRINT:
+    case TAC_ARG:
+    case TAC_CALL:
+    case TAC_IFZ:
+    case TAC_IFN:
+    case TAC_LABEL:
+    case TAC_JMP:
+    case TAC_SUB:
     case TAC_ADD:
-      // sei la 
+    case TAC_MUL:
+    case TAC_DIV:
+    case TAC_INC:
+    case TAC_PARAM:
+    case TAC_VARDEC:
+    case TAC_VECDEC:
+    case TAC_OR:
+    case TAC_EQUAL:
+    case TAC_NOTEQUAL:
+    case TAC_LESSEQUAL:
+    case TAC_GREATEREQUAL:
+    case TAC_LESS:
+    case TAC_GREATER:
+    case TAC_AND:
       break;
     }
 }
